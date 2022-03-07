40cd91f516c5fd6a1f5a87a62a2f4bc512852851
AdrianValladares <a.valladares.d@gmail.com>
2022-03-08 00:02:51 +0100

    installed sonarqube

--- __tests__/pages/index.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../pages/index";
-
-describe("index", () => {
-  it("renders a paragraph", () => {});
-});
---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,5 @@
+describe("index", () => {
+  it("renders a paragraph", () => {});
+});
+
+export {};
