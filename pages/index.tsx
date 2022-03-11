624fb89287a7d1d5af5846bd7564e4e2f27d0832
AdrianValladares <a.valladares.d@gmail.com>
2022-03-11 09:34:56 +0100

    updated index.tsx in root

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import type { NextPage } from "next";
-import styled from "styled-components";
-
-const StyledP = styled.article`
-  font-family: ${(props) => props.theme.logoFont};
-`;
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return <div> </div>;
 };
 
 export default Home;
