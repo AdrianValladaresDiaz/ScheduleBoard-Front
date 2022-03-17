04874349d18a6c2bc05db5dbf768f48897b3e058
AdrianValladares <a.valladares.d@gmail.com>
2022-03-17 15:52:43 +0100

    redirect from /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
   return <div> </div>;
 };
 
