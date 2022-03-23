d915bb3495d121c1bef06558b505dc100dd5da36
AdrianValladares <a.valladares.d@gmail.com>
2022-03-23 21:38:06 +0100

    tested login form with cypress

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -6,15 +6,26 @@ describe("App", function () {
     cy.visit("/login");
   });
 
-  it("should find the 1st input and type kevin", function () {
+  it("should find the 1st input and type mario@mario.com", function () {
     cy.get("input")
       .first()
       .type("mario@mario.com")
       .should("have.value", "mario@mario.com");
   });
 
-  it("should find the 2md input and type kevin", function () {
+  it("should find the 2md input and type papaya", function () {
     cy.get("input").last().type("papaya").should("have.value", "papaya");
+  });
+
+  it("should login", function () {
+    cy.get("input")
+      .first()
+      .type("mario@mario.com")
+      .should("have.value", "mario@mario.com");
+
+    cy.get("input").last().type("papaya").should("have.value", "papaya");
+
+    cy.contains("Log me in").click();
   });
 });
 
